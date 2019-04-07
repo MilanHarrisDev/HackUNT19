2839293a92020e77eb2cc9bb8838c336853c93e5
Lathe Abusaid <labusaid@gmail.com>
2019-04-06 20:25:03 -0500

    merged dumb caption thing

---
+++ js/speechRec.js
@@ -0,0 +1,25 @@
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
+const recognition = new SpeechRecognition();
+var text = "";
+
+$( document ).ready(function() {
+
+    recognition.iterimResults = true;
+
+    recognition.addEventListener("result", e => {
+        const transcript = Array.from(e.results)
+        .map(result => result.transcript).join("")
+
+        // console.log(e.results[0][0].transcript);
+        // text = e.results[0][0].transcript;
+        $('#caption').text(e.results[0][0].transcript);
+    });
+
+
+
+});
+
+recognition.start();
+
+recognition.addEventListener("end", recognition.start);
