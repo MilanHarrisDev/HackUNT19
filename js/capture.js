d40b7994d56f030adf34f9511d3ae9ff888b007e
Milan Harris <milanharrisdev@gmail.com>
2019-04-06 19:31:42 -0500

    back camera probably fixed?

--- js/capture.js
+++ js/capture.js
@@ -3,10 +3,12 @@ const video = document.getElementById('video');
 function startup() {
   navigator.mediaDevices.getUserMedia({
     audio: false,
-    video: true,
-    facingMode: { 
-      exact: 'user'
-    }
+    video: {
+      facingMode: { 
+        exact: 'user'
+      }
+    },
+    
   }).then(stream => {
     video.srcObject = stream;
   }).catch(console.error)
