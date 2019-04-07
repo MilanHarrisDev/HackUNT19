5894d8e801d94145f750f884c31d59423407d2c2
Milan Harris <milanharrisdev@gmail.com>
2019-04-06 23:41:16 -0500

    Added Zoom functionality

--- js/capture.js
+++ js/capture.js
@@ -1,4 +1,25 @@
 const video = document.getElementById('video');
+const zooms = [0, 10, 25, 50, 75, 100];
+var currentZoom = 0;
+
+$( document ).ready(function() {
+  $("#zoom-button").click(function() {
+    currentZoom++;
+    if(currentZoom >= zooms.length){
+      currentZoom = 0;
+    }
+
+    $(this).text(colorModes[currentMode]);
+    $("#video").css("width", (100 + zooms[currentZoom]).toString() + "%");
+    $("#video").css("height", (100 + zooms[currentZoom]).toString() + "%");
+    $("#video").css("top", (zooms[currentZoom] / -2.0).toString() + "%");
+    $("#video").css("left", (zooms[currentZoom] / -2.0).toString() + "%");
+    $(this).text((100 + zooms[currentZoom]).toString() + "%");
+
+    console.log("top: " + (zooms[currentZoom] / 2.0).toString() + "%");
+    console.log("bottom: " + (zooms[currentZoom] / 2.0).toString() + "%");
+  });
+});
 
 function startup() {
   navigator.mediaDevices.getUserMedia({
